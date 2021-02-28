1892b22bd1ed89e821e9504f9f9fa553614572de
Siddhant Chaudhary <59536395+codetalker7@users.noreply.github.com>
2021-03-01 00:58:52 +0530

    Add files via upload

---
+++ version.php
@@ -0,0 +1,11 @@
+/*
+    this file contains the version
+    number for the website. For each file,
+    simply pass on a query string ?version = VERSION
+    to the file's url. This is to make sure that 
+    every time a user open a file, he opens the fresh 
+    version
+*/
+<?php 
+    $VERSION = "1.0.1";
+?>
